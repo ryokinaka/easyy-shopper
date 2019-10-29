66d70cc143d92d3d15c9367ba11c3a09477b2e1b
ryoki.naka <dfghklmjkm@gmail.com>
2019-10-29 13:01:17 +0000

    user login

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  before_save { self.email.downcase! }
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :email, presence: true, length: { maximum: 40 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    uniqueness: { case_sensitive: false }
+  validates :address, length: { maximum: 60 }
+  has_secure_password
+end
