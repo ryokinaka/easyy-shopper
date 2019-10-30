29e82b741ca3d9a67064c060ffa20a33aa7387c4
ryoki.naka <dfghklmjkm@gmail.com>
2019-10-30 07:27:12 +0000

    wants

--- app/controllers/wants_controller.rb
+++ app/controllers/wants_controller.rb
@@ -1,6 +1,6 @@
 class WantsController < ApplicationController
   def index
-    @wants = Want.all.page(params[:page])
+    @wants = current_user.wants.all.page(params[:page]).per(10)
   end
 
   def create
