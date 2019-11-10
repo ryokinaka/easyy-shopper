8a35c91b9e19f08687e684bd6a219debcb1673b4
ryoki.naka <dfghklmjkm@gmail.com>
2019-11-10 02:53:22 +0000

    modification

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,13 @@ class ItemsController < ApplicationController
   before_action :require_user_logged_in
     
   def index
+    if logged_in?
+      @items = Item.all.order(id: :desc).page(params[:page]).per(20)
+
+      if params[:category].present?
+        @items = Item.page(params[:page]).per(20).get_by_category params[:category]
+      end
+    end
   end
     
   def show
